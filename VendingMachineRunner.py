f2e01f1e97cb7ce319668fec870dfa4a4345b561
SNOW <kancth03@gmail.com>
2023-05-02 20:54:40 +0900

    Refactor: 프로젝트 구조 변경

--- VendingMachineRunner.py
+++ VendingMachineRunner.py
@@ -6,26 +6,46 @@ import json
 
 vm_window = Tk()
 vm_window.title("자판기")
-vm_window.config()
 # 창의 초기 생성위치 설정
 vm_window.geometry("+700+300")
 
-logoImg = ImageTk.PhotoImage(file="images/vending-machine.png")
-vmImageSize = 256
-canvas = Canvas(width=vmImageSize, height=vmImageSize, highlightthickness=0)
-canvas.create_image(int(vmImageSize/2 + 1), int(vmImageSize/2 + 1), image=logoImg)
-canvas.grid(column=2, row=0)
+imgSize = 64
+img = ImageTk.PhotoImage(file="images/drink.png")
+canvas = Canvas(width=imgSize, height=imgSize, highlightthickness=0)
+canvas.create_image(32, 32, image=img)
+canvas.grid(row=0, column=0)
 
 tempButton1 = Button(text="임시 버튼1")
-tempButton1.grid(column=0, row=1)
-tempButton2 = Button(text="임시 버튼2")
-tempButton2.grid(column=1, row=1)
-tempButton3 = Button(text="임시 버튼3")
-tempButton3.grid(column=2, row=1)
-tempButton4 = Button(text="임시 버튼4")
-tempButton4.grid(column=3, row=1)
-tempButton5 = Button(text="임시 버튼5")
-tempButton5.grid(column=4, row=1)
+tempButton1.grid(row=1, column=0)
 
 
 vm_window.mainloop()
+
+# try:
+#     with open("drink_list.json", "r") as drink_list_file:
+#         data = json.load(drink_list_file)
+#
+# except FileNotFoundError:
+#     with open("drink_list.json", "w") as drink_list_file:
+#         data = {
+#
+#         }
+#         json.dump(data, drink_list_file, indent=4)
+#
+# else:
+#     while True:
+#         drink_name = input("음료수 이름을 입력하세요: ")
+#         if drink_name == "break":
+#             break
+#         drink_price = int(input("음료수 가격을 입력하세요: "))
+#         drink_num = int(input("음료수 재고를 입력하세요: "))
+#         new_data = {
+#             drink_name: {
+#                 "가격": drink_price,
+#                 "재고": drink_num
+#             }
+#         }
+#         data.update(new_data)
+#
+#         with open("drink_list.json", "w") as drink_list_file:
+#             json.dump(data, drink_list_file, indent=4, ensure_ascii=False)
