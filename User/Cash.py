6dd1891a0cf64deca991bf24da209f44367f276c
SNOW <kancth03@gmail.com>
2023-05-09 09:19:32 +0900

    Feat: User, Manager 돈 관련 데이터 관리 파일 생성 및 Cash 파일에 연동

--- User/Cash.py
+++ User/Cash.py
@@ -1,27 +1,24 @@
 from CustomException import *
+import json
 
 
 class Cash:
 
     def __init__(self):
-        self.__Cash = {
-            5000: 0,
-            1000: 0,
-            500: 0,
-            100: 0
-        }
+        with open("user_wallet.json", "r") as file:
+            self.Cash = json.load(file)
 
     def set_cash(self, money_type, how_much):
         if money_type == 5000 or money_type == 1000 or money_type == 500 or money_type == 100:
             if (type(how_much) == int or how_much.isdigit()) and how_much >= 0:
                 if money_type == 5000:
-                    self.__Cash[5000] = how_much
+                    self.Cash[5000] = how_much
                 elif money_type == 1000:
-                    self.__Cash[1000] = how_much
+                    self.Cash[1000] = how_much
                 elif money_type == 500:
-                    self.__Cash[500] = how_much
+                    self.Cash[500] = how_much
                 elif money_type == 100:
-                    self.__Cash[100] = how_much
+                    self.Cash[100] = how_much
                 else:
                     raise ArgumentException("Cash.set_cash argument exception")
         else:
@@ -30,8 +27,8 @@ class Cash:
     def total_money(self):
         # TODO 가지고 있는 지폐를 금액으로 환산하여 값을 리턴
         total = 0
-        total += self.__Cash[5000] * 5000
-        total += self.__Cash[1000] * 1000
-        total += self.__Cash[500] * 500
-        total += self.__Cash[100] * 100
+        total += self.Cash[5000] * 5000
+        total += self.Cash[1000] * 1000
+        total += self.Cash[500] * 500
+        total += self.Cash[100] * 100
         return total
